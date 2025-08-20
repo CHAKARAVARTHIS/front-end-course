1d6e6c31d77fee8ea073ef2be4a821dbf2cfda1e
CHAKARAVARTHIS <chakaravarthikumar@gmail.com>
2025-08-20 18:10:31 +0530

    fifth  commit

--- form test/script.js
+++ form test/script.js
@@ -20,16 +20,25 @@
 //   myform.reset();
 // });
  
-let mydata=document.getElementById("form");
+let mydata=document.getElementById('contactform');
 mydata.addEventListener("submit",(event)=>{
   event.preventDefault();
 
-  let fullname=document.getElementById('name').value;
-  let mail=document.getElementById('email').values;
-  let password=document.getElementById('password').values;
-  let confirm=document.getElementById('confirm').values;
+  let username=document.getElementById('name');
+  let email=document.getElementById('email');
+  let password=document.getElementById('password');
+  let confirmp=document.getElementById('confirm');
 
- 
   document.querySelectorAll(".error").forEach(er=>er.textContent="");
-  document.querySelectorAll(".errors").forEach(er =>er.classList.remove("error"));
+  document.querySelectorAll(".errors").forEach(er=>er.classList.remove="error");
+   let valid=true;
+   if(username.value.trim()===""){
+    document.getElementById("error-name").textcontent="please fill your namme";
+    username.classList.add("error");
+    valid=false;
+   }
+  if (valid) {
+            const data = {
+                name: username.value.trim()};
+            }
 })
