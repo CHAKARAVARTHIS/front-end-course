4936264576f9d8b16d7115cf3217bad2ef3f9b16
CHAKARAVARTHIS <chakaravarthikumar@gmail.com>
2025-09-02 12:47:40 +0530

    commit

---
+++ food/src/Chakara.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Anyname =()=>{
+return(
+   <section>
+   <div>
+   <h1>hellon</h1>
+   <span> notice</span>
+   <span> collections</span>
+   </div>
+   </section>
+)
+};
+export default Anyname;
---
+++ food/src/chakara.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Chakara(){
+    return(
+        <div><h1>hello world</h1>
+        <p>scratch la irrunthu arambikuren</p>
+        </div>
+    )
+       
+}; 
+export default Chakara;
